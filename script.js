d2c00435e361494132a42d50928a01dcac273fc4
Bhautik Vekariya <vbhautik41@gmail.com>
2026-01-16 15:43:25 +0530

    page1 done

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ tl.from(".line h1",{
 tl.from(".line1-part1",{
     opacity:0,
     onStart:function(){
+
+
         
 
 let h5timer = document.querySelector(".line1-part1 h5");
@@ -35,19 +37,39 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:4,
+    delay:0,
 })
 tl.from("#page1",{
     opacity:0,
     delay:0.2,
     y:1600,
     ease:Power4,
-    duration:0.5,
+    duration:0.6,
 })
 
 tl.to("#loader",{
     display:"none"
 })
+
+tl.from("#nav",{
+    opacity:0,
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2,
+})
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y,
+    
+        })
+    })
+    
+    Shery.makeMagnet("#nav h4");
 }
 
 loadingAnimation();
+cursorAnimation();
